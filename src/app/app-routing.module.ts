165cd0ee0f6e88b4d1bcbd566201088e196a73c5
Hari-GIT1 <nagellyhariharan@gmail.com>
2024-11-28 11:21:59 +0530

    Login view completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { EmployeesComponent } from './components/employees/employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  {path:"",component:LoginComponent},
+  {path:"dashboard",component:DashboardComponent,children:[
+    {path:"home",component:HomeComponent},
+    {path:"create",component:CreateEmployeeComponent},
+    {path:"details",component:EmployeesComponent},
+  ]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
